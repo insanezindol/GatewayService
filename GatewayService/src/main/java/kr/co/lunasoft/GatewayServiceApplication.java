4c2881b2109e24f9f5986df117f89d0fb8fc0859
딘 <jhlee@lunasoft.co.kr>
2020-01-16 18:42:27 +0900

    change host

--- GatewayService/src/main/java/kr/co/lunasoft/GatewayServiceApplication.java
+++ GatewayService/src/main/java/kr/co/lunasoft/GatewayServiceApplication.java
@@ -3,8 +3,10 @@ package kr.co.lunasoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
 
